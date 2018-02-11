eaf62c047f76d52f1541f6c66bd7ea39cd1dfb70
Austin <austin@legitpatents.com>
2018-02-11 11:16:44 -0500

    all

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+       apiKey: "AIzaSyAcS99KKVW59yGss0x0uc2DTWITc1qQVtQ",
+    authDomain: "cuffy-6092c.firebaseapp.com",
+    databaseURL: "https://cuffy-6092c.firebaseio.com",
+    projectId: "cuffy-6092c",
+    storageBucket: "cuffy-6092c.appspot.com",
+    messagingSenderId: "677438883910"
+    },
+    googleMapsKey: 'AIzaSyAe7mnGxXmMBwf3YF6R2EzGBsh-86W8jJ4',
+    };
